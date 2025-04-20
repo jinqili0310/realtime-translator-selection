6f3d1770e76f336054a248b597041093d059c700
jinqili0310 <jinqi.li.310@gmail.com>
2025-04-20 12:09:28 -0700

    ignore linter errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    // !! WARN !!
+    // Ignoring TypeScript errors during build
+    // This is not recommended unless you're aware of the implications
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // !! WARN !!
+    // Ignoring ESLint errors during build
+    // This is not recommended for production code quality
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
